5baf1ad955e2ac92be7cab4e81205deed4034dfe
i'DLisT <i.dlist@outlook.com>
2022-03-04 11:14:17 +0800

    fix: context

--- plugins.scoped/s1coders.js
+++ plugins.scoped/s1coders.js
@@ -1,11 +1,7 @@
-const { s1coders, adminGroup } = require('../koishi.secret')
-
 /**
  * @param {import('koishi').Context} ctx
  */
 module.exports = ctx => {
-  ctx = ctx.platform('onebot').guild(s1coders, ...adminGroup)
-
   ctx.plugin(require('./gacha'))
   ctx.plugin(require('./tigang'))
 }
