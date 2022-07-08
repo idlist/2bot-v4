15f586a3d92f8a253fb25e85b8a92253e7472323
i'DLisT <i.dlist@outlook.com>
2022-07-08 17:11:09 +0800

    feat: add new silly

--- plugins/fun/silly.js
+++ plugins/fun/silly.js
@@ -18,6 +18,7 @@ Promise.all([
   readTextFile('data/silly.2019.txt'),
   readTextFile('data/silly.2020.txt'),
   readTextFile('data/silly.2021.txt'),
+  readTextFile('data/silly.2022firsthalf.txt'),
 ])
 
 /**
