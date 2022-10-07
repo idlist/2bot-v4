ebde4db119d4c6295b9665f0f879198a9d2d74bc
i'DLisT <i.dlist@outlook.com>
2022-10-07 17:13:12 +1100

    fix: script logic

--- scripts/migrate-segment.js
+++ scripts/migrate-segment.js
@@ -18,12 +18,13 @@ const migrate = async () => {
   const res = await query('SELECT id, answer FROM dialogue WHERE answer like \'[CQ:image%\'')
 
   const modified = [...res]
-  modified.forEach(async (item) => {
+
+  for await (const item of modified) {
     const newImage = item.answer.replace(/\[CQ:image,url=(.+)\]/, '<image url="$1">')
 
     await query('UPDATE dialogue SET answer = ? WHERE id = ?', [newImage, item.id])
     console.log(`Migrated question ID ${item.id}.`)
-  })
+  }
 
   console.log('Migrate finished.')
   pool.end()
